a011a2d370407bfca867fb3b848761a683dcbb36
cmeza432 <cmeza432@gmail.com>
2019-04-25 09:57:24 -0500

    Added Names and comments

--- src/junit/testing/JunitTesting.java
+++ src/junit/testing/JunitTesting.java
@@ -1,13 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+JUnit project for software testing.
+Name:   Carlos Meza, David Benepe
+*/
+
 package junit.testing;
 
 /**
  *
- * @author Carlos Meza
+ * @author Carlos Meza and David Benepe
  */
 public class JunitTesting {
 
