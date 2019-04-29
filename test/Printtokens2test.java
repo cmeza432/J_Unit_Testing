172589f9dc43def1b9ff793315b84568feb498a4
Carlos Meza <cmeza432@gmail.com>
2019-04-29 09:59:08 -0500

    Updated test file, under test packages

---
+++ test/Printtokens2test.java
@@ -0,0 +1,15 @@
+/*
+    Junit test file for printtoken2 java file.
+    Project for Software Testing
+    By: Carlos Meza, David Benepe 
+*/
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Printtokens2test {
+
+    @Test
+    public void test_open_character_stream(){
+        fail("Not yet implemented!");
+    }
+}
